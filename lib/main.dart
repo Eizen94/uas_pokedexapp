29cd0fe8d46160916f6a146fca71925250205554
Eizen94 <klausi_lanthaler@outlook.com>
2024-12-16 23:56:08 +0700

    auto: update at 2024-12-16 23:56:08

--- lib/main.dart
+++ lib/main.dart
@@ -114,7 +114,7 @@ class PokedexApp extends StatelessWidget {
       ),
       builder: (context, child) {
         return MediaQuery(
-          data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+          data: MediaQuery.of(context).copyWith(textScaler: TextScaler.linear(1.0)),
           child: child ?? const SizedBox.shrink(),
         );
       },
