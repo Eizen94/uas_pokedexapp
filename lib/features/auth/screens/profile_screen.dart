9cceb273cdf373a52206550eed376803c763562b
Eizen94 <klausi_lanthaler@outlook.com>
2024-12-19 19:35:33 +0700

    auto: update at 2024-12-19 19:35:32

--- lib/features/auth/screens/profile_screen.dart
+++ lib/features/auth/screens/profile_screen.dart
@@ -6,6 +6,7 @@ library;
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import '../../../core/constants/colors.dart';
 import '../../../core/constants/text_styles.dart';
