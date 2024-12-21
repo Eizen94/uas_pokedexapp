519422662dd160502f67daee26b3ec00d428f538
Eizen94 <klausi_lanthaler@outlook.com>
2024-12-21 22:58:26 +0700

    auto: update at 2024-12-21 22:58:25

--- lib/features/favorites/services/favorite_service.dart
+++ lib/features/favorites/services/favorite_service.dart
@@ -1,7 +1,7 @@
 // lib/features/favorites/services/favorite_service.dart
 
 import 'dart:async';
-import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:flutter/foundation.dart';
 
 import '../../../core/config/firebase_config.dart';
